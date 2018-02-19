6a6e685d9051060b77370809e0e0ec4a160912d3
JoshCaravaggio <Joshua@Joshuas-MacBook-Pro-3.local>
2018-02-19 17:01:54 -0500

    A2 Beginnings

--- testing/A1Testing/testHarness2/.depend/GEDCOMparser.d
+++
@@ -1,3 +0,0 @@
-bin/GEDCOMparser.o: studentCode/GEDCOMparser.c \
-  studentInclude/LinkedListAPI.h include/GEDCOMparser.h \
-  include/LinkedListAPI.h studentInclude/GEDCOMutilities.h
--- testing/A1Testing/testHarness2/.depend/TestFunctions.d
+++
@@ -1,3 +0,0 @@
-bin/TestFunctions.o: src/TestFunctions.c include/GEDCOMparser.h \
-  include/LinkedListAPI.h include/GEDCOMTestUtilities.h \
-  include/TestHarness.h include/TestFunctions.h
